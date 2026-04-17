5193ebf002d1ed967436293fdd4e0ecf5c28f275
Varad Sane <varadsane2004@gmail.com>
2026-04-17 22:50:16 +0530

    Updated API base URL for deployment

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://complaint-management-1j73.onrender.com",
   withCredentials: true
 });
 
